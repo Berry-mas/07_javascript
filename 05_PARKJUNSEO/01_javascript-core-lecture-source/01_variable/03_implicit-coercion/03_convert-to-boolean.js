52123cef9fb2b93e8d74a9d7e7d7a7162d167083
junseo <junseo9145@gmail.com>
2025-05-07 09:12:37 +0900

    20250507 수업 시작 커밋

--- 05_PARKJUNSEO/01_javascript-core-lecture-source/01_variable/03_implicit-coercion/03_convert-to-boolean.js
+++ 05_PARKJUNSEO/01_javascript-core-lecture-source/01_variable/03_implicit-coercion/03_convert-to-boolean.js
@@ -3,13 +3,13 @@
 // 또는 Falsy(거짓으로 평가되는 값)으로 구분한다.
 // Truthy -> true, Falsy -> false로 암묵적 타입 변환이 된다.
 
-if (undefined) console.log('if(undefined)');    // false
-if (null) console.log('if(null)');  // false
-if (0) console.log('if(0)');    // false
-if (1) console.log('if(1)');    // true
-if (NaN) console.log('if(NaN)');    // false
-if('') console.log(`if('')`);   // false
-if('l') console.log('if(`l`)');
+if (undefined) console.log("if(undefined)"); // false
+if (null) console.log("if(null)"); // false
+if (0) console.log("if(0)"); // false
+if (1) console.log("if(1)"); // true
+if (NaN) console.log("if(NaN)"); // false
+if ("") console.log(`if('')`); // false
+if ("l") console.log("if(`l`)");
 
 // 결론 : undefined, null, 0, NaN, ''(빈 문자열)은 Falsy한 값이몌,
 //       이외의 모든 값들은 Truthy값이다.
---
+++ 05_PARKJUNSEO/homework_PARKJUNSEO/99_answer/01_array-and-built-in-object-answer/01_02_answer.js
@@ -0,0 +1,14 @@
+function filterRange(arr, a, b) {
+  if (arr.indexOf(a) > arr.indexOf(b)) {
+    const leftIndex = arr.indexOf(b)
+    const rigthIndex = arr.indexOf(b)
+  }
+  for (let i = )
+}
+
+
+const arr = [5, 3, 8, 1, 10, 4];
+
+filterRange(arr, 1, 5);
+
+console.log(arr);
